7a40caf02d50180b74ccdc8a9648b3460527ee62
xicook <xicohoffmann@naturesafe.com.br>
2026-02-23 15:32:17 -0300

    Fix: Ultimate VGA restoration with font salvage and canonical registers

--- drivers/vga_graphics.c
+++ drivers/vga_graphics.c
@@ -13,6 +13,19 @@
 #define VGA_CRTC_DATA 0x3D5
 #define VGA_INSTAT_READ 0x3DA
 
+static uint8_t saved_font[4096];
+static int font_saved = 0;
+
+static void outb(uint16_t port, uint8_t val) {
+  asm volatile("outb %0, %1" : : "a"(val), "dN"(port));
+}
+
+static uint8_t inb(uint16_t port) {
+  uint8_t val;
+  asm volatile("inb %1, %0" : "=a"(val) : "dN"(port));
+  return val;
+}
+
 static void write_regs(uint8_t *regs) {
   // Write MISC
   asm volatile("outb %0, %1" : : "a"(*(regs++)), "dN"(VGA_MISC_WRITE));
@@ -128,7 +141,7 @@ uint8_t mode_13h_regs[] = {
 uint8_t mode_text_regs[] = {
     /* MISC */ 0x67,
     /* SEQ */ 0x03,
-    0x00,
+    0x01,
     0x03,
     0x00,
     0x02,
@@ -188,28 +201,106 @@ uint8_t mode_text_regs[] = {
     0x08,
     0x00};
 
+void vga_save_font() {
+  // Save state
+  outb(VGA_GC_INDEX, 0x04);
+  uint8_t old_gc4 = inb(VGA_GC_DATA);
+  outb(VGA_GC_INDEX, 0x05);
+  uint8_t old_gc5 = inb(VGA_GC_DATA);
+  outb(VGA_GC_INDEX, 0x06);
+  uint8_t old_gc6 = inb(VGA_GC_DATA);
+  outb(VGA_SEQ_INDEX, 0x02);
+  uint8_t old_seq2 = inb(VGA_SEQ_DATA);
+  outb(VGA_SEQ_INDEX, 0x04);
+  uint8_t old_seq4 = inb(VGA_SEQ_DATA);
+
+  // Switch to font read mode
+  outb(VGA_SEQ_INDEX, 0x04);
+  outb(VGA_SEQ_DATA, 0x07); // Sequential
+  outb(VGA_SEQ_INDEX, 0x02);
+  outb(VGA_SEQ_DATA, 0x04); // Write only to plane 2
+  outb(VGA_GC_INDEX, 0x04);
+  outb(VGA_GC_DATA, 0x02); // Read only from plane 2
+  outb(VGA_GC_INDEX, 0x05);
+  outb(VGA_GC_DATA, 0x00); // Read mode 0, write mode 0
+  outb(VGA_GC_INDEX, 0x06);
+  outb(VGA_GC_DATA, 0x00); // Map to 0xA0000
+
+  uint8_t *vga_mem = (uint8_t *)0xA0000;
+  for (int i = 0; i < 4096; i++) {
+    saved_font[i] = vga_mem[i];
+  }
+  font_saved = 1;
+
+  // Restore state
+  outb(VGA_GC_INDEX, 0x04);
+  outb(VGA_GC_DATA, old_gc4);
+  outb(VGA_GC_INDEX, 0x05);
+  outb(VGA_GC_DATA, old_gc5);
+  outb(VGA_GC_INDEX, 0x06);
+  outb(VGA_GC_DATA, old_gc6);
+  outb(VGA_SEQ_INDEX, 0x02);
+  outb(VGA_SEQ_DATA, old_seq2);
+  outb(VGA_SEQ_INDEX, 0x04);
+  outb(VGA_SEQ_DATA, old_seq4);
+}
+
+void vga_restore_font() {
+  if (!font_saved)
+    return;
+
+  // Switch to font write mode
+  outb(VGA_SEQ_INDEX, 0x02);
+  outb(VGA_SEQ_DATA, 0x04); // Write only to plane 2
+  outb(VGA_SEQ_INDEX, 0x04);
+  outb(VGA_SEQ_DATA, 0x07); // Sequential
+  outb(VGA_GC_INDEX, 0x05);
+  outb(VGA_GC_DATA, 0x00); // Read mode 0, write mode 0
+  outb(VGA_GC_INDEX, 0x06);
+  outb(VGA_GC_DATA, 0x00); // Map to 0xA0000
+
+  uint8_t *vga_mem = (uint8_t *)0xA0000;
+  for (int i = 0; i < 4096; i++) {
+    vga_mem[i] = saved_font[i];
+  }
+
+  // Back to text mode memory layout
+  outb(VGA_SEQ_INDEX, 0x02);
+  outb(VGA_SEQ_DATA, 0x03); // Enable planes 0 & 1
+  outb(VGA_SEQ_INDEX, 0x04);
+  outb(VGA_SEQ_DATA, 0x02); // Odd/Even
+  outb(VGA_GC_INDEX, 0x06);
+  outb(VGA_GC_DATA, 0x0E); // Map to 0xB8000
+}
+
 void vga_set_mode13h() { write_regs(mode_13h_regs); }
 
 void vga_set_text_mode() {
-  // Reset sequencer for stable mode switch
-  asm volatile("outb %0, %1" : : "a"((uint8_t)0x00), "dN"(VGA_SEQ_INDEX));
-  asm volatile("outb %0, %1" : : "a"((uint8_t)0x01), "dN"(VGA_SEQ_DATA));
+  // Sync wait
+  while (inb(VGA_INSTAT_READ) & 0x08)
+    ;
+  while (!(inb(VGA_INSTAT_READ) & 0x08))
+    ;
+
+  // Reset sequencer
+  outb(VGA_SEQ_INDEX, 0x00);
+  outb(VGA_SEQ_DATA, 0x01);
 
   write_regs(mode_text_regs);
+  vga_restore_font();
 
   // Restart sequencer
-  asm volatile("outb %0, %1" : : "a"((uint8_t)0x00), "dN"(VGA_SEQ_INDEX));
-  asm volatile("outb %0, %1" : : "a"((uint8_t)0x03), "dN"(VGA_SEQ_DATA));
+  outb(VGA_SEQ_INDEX, 0x00);
+  outb(VGA_SEQ_DATA, 0x03);
 
-  // Enable display and reset AC index
-  uint8_t val;
-  asm volatile("inb %1, %0" : "=a"(val) : "dN"(VGA_INSTAT_READ));
-  asm volatile("outb %0, %1" : : "a"((uint8_t)0x20), "dN"(VGA_AC_INDEX));
+  // Reset Attribute Controller
+  inb(VGA_INSTAT_READ);
+  outb(VGA_AC_INDEX, 0x20);
 
-  // Clear text buffer and set default gray on black
+  // Clear text buffer
   uint16_t *fb = (uint16_t *)0xB8000;
   for (int i = 0; i < 80 * 25; i++) {
-    fb[i] = 0x0720; // Space ' ', Light Gray on Black
+    fb[i] = 0x0720;
   }
 }
 
--- drivers/vga_graphics.h
+++ drivers/vga_graphics.h
@@ -5,6 +5,7 @@
 
 void vga_set_mode13h();
 void vga_set_text_mode();
+void vga_save_font();
 void vga_plot_pixel(int x, int y, uint8_t color);
 void vga_clear_graphics(uint8_t color);
 
